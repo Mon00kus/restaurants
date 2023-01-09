57b6e6dbdf0014810977c8bd266e81171cfd345a
Mon00kus <raulmoncadac@gmail.com>
2023-01-09 08:10:13 -0400

    Generacion formulario
    Generacion formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="container mt-5">
       <h1>Task</h1>
       <hr/>
+        <div className='row'>
+          <div className='col-8'>
+            <h4>Lista de Tareas</h4>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                <span className='lead'>Nombre de la tarea</span>
+                <button className='btn btn-danger btn-sm float-right mx-2'>Eliminar</button>
+                <button className='btn btn-warning btn-sm float-right'>Editar</button>
+              </li>
+            </ul>
+          </div>
+          <div className='col-4'>
+            <h4 className='text-center'>Formulario</h4>
+            <form>
+              <input className='form-control mb-2' type="text" placeholder='Ingrese la tarea'>
+              </input>
+              <button className='btn btn-dark btn-block' type="submit">Agregar</button>
+            </form>
+          </div>
+        </div>
     </div>
   );
 }
