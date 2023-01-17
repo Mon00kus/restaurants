284dd5db1419036ca6ea25df7c5a15eb00af208d
Mon00kus <raulmoncadac@gmail.com>
2023-01-16 23:12:35 -0400

    Cambios 20231611
    Se implemento la lista dinamica de tareas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { isEmpty } from 'lodash';
+import shortid from 'shortid';
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const addTask = (e) => {
+    e.preventDefault();
+    if (isEmpty(task)){
+      console.log("Task empty");
+      return;
+    }
+    //console.log("Ok");
+    const newTask = {
+        id : shortid.generate(),
+        name : task
+    };
+    setTasks([...tasks,newTask]);
+    setTask("");
+  };
   return (
     <div className="container mt-5">
-      <h1>Task</h1>
+      <h1>Tareas</h1>
       <hr/>
         <div className='row'>
           <div className='col-8'>
             <h4>Lista de Tareas</h4>
             <ul className='list-group'>
-              <li className='list-group-item'>
-                <span className='lead'>Nombre de la tarea</span>
+            {
+              tasks.map((task)=>(
+                <li className='list-group-item' key={task.id}>
+                <span className='lead'>{task.name}</span>
                 <button className='btn btn-danger btn-sm float-right mx-2'>Eliminar</button>
                 <button className='btn btn-warning btn-sm float-right'>Editar</button>
               </li>
+              ))
+            }
             </ul>
           </div>
           <div className='col-4'>
             <h4 className='text-center'>Formulario</h4>
-            <form>
-              <input className='form-control mb-2' type="text" placeholder='Ingrese la tarea'>
-              </input>
-              <button className='btn btn-dark btn-block' type="submit">Agregar</button>
+            <form onSubmit={addTask}>
+              <input 
+                  className='form-control mb-2' 
+                  type="text" 
+                  placeholder='Ingrese la tarea'
+                  onChange={(text) => setTask(text.target.value)}
+                  value={task}
+               />
+               <button 
+                  className='btn btn-dark btn-block' 
+                  type="submit">Agregar
+               </button>
             </form>
           </div>
         </div>
