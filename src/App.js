25c960bb1463f0b4556511bc8d6b4980538823dd
Mon00kus <raulmoncadac@gmail.com>
2023-02-02 22:22:46 -0400

    Firebase
    Se implemento el acceso a bd firebase trayendo id y name de tareas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { isEmpty, size } from 'lodash';
 import shortid from 'shortid';
+import { getCollection } from './actions';
 
 function App() {
 
@@ -10,11 +11,20 @@ function App() {
   const [id, setId] = useState("");
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      const result = await getCollection("tasks");
+      //console.log(result);
+      setTasks(result.data);
+    })();
+  }, []);
+
+
   const validForm = () => {
     let isValid = true;
     setError(null);
 
-    if (isEmpty(task)){
+    if (isEmpty(task)) {
 
       if (!editMode)
         setError("Debes ingresar una tarea.");
@@ -23,37 +33,37 @@ function App() {
 
       isValid = false;
 
-    }  
+    }
 
     return isValid;
   };
 
   const addTask = (e) => {
-    e.preventDefault();   
-    
-    if (!validForm()){
-      return;
-    }
-    
-    const newTask = {
-      id : shortid.generate(),
-      name : task
-    };
-    
-    setTasks([...tasks,newTask]);
-    setTask("");
-  };
-  
-  const saveTask = (e) => {
     e.preventDefault();
-    
-    if (!validForm()){
+
+    if (!validForm()) {
       return;
     }
 
-    const editedTasks = tasks.map(item => item.id === id ? {id, name: task} : item );
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask("");
+  };
+
+  const saveTask = (e) => {
+    e.preventDefault();
+
+    if (!validForm()) {
+      return;
+    }
+
+    const editedTasks = tasks.map(item => item.id === id ? { id, name: task } : item);
     setTasks(editedTasks);
-    setEditmode (false);
+    setEditmode(false);
     setTask("");
     setId("");
   };
@@ -73,59 +83,59 @@ function App() {
 
     <div className="container mt-5">
       <h1>Tareas</h1>
-      <hr/>
-        <div className='row'>
-          <div className='col-8'>
-            <h4 className='text-center'>Lista de Tareas</h4>
-            {
-              size(tasks) === 0  ? (
-                  <li className='list-group-item'>Aun no hay Tareas programadas.</li>
-              ) : (
-                <ul className='list-group'>            
+      <hr />
+      <div className='row'>
+        <div className='col-8'>
+          <h4 className='text-center'>Lista de Tareas</h4>
+          {
+            size(tasks) === 0 ? (
+              <li className='list-group-item'>Aun no hay Tareas programadas.</li>
+            ) : (
+              <ul className='list-group'>
                 {
                   tasks.map((task) => (
                     <li className='list-group-item' key={task.id}>
-                    <span className='lead'>{task.name}</span>
-                    <button 
+                      <span className='lead'>{task.name}</span>
+                      <button
                         className='btn btn-danger btn-sm float-right mx-2'
-                        onClick={()=>deleteTask(task.id)}
-                    >
-                      Eliminar</button> 
-                    <button className='btn btn-warning btn-sm float-right'
-                        onClick={()=>editTask(task)}
-                    >
-                      Editar</button>
-                  </li>
-                  ))                   
+                        onClick={() => deleteTask(task.id)}
+                      >
+                        Eliminar</button>
+                      <button className='btn btn-warning btn-sm float-right'
+                        onClick={() => editTask(task)}
+                      >
+                        Editar</button>
+                    </li>
+                  ))
                 }
-                </ul>
-              )                                        
-            }
-          </div>
-          <div className='col-4'>
-            <h4 className='text-center'
-              > { editMode ? "Modificar Tarea" : "Agregar Tarea" }
-            </h4>
-            <form onSubmit={editMode ? saveTask : addTask}>
-              {
-               error && <span className='text-danger'>{error}</span>
-              }
-              <input 
-                  className='form-control mb-2' 
-                  type="text" 
-                  placeholder='Ingrese la tarea'
-                  onChange={(text) => setTask(text.target.value)}
-                  value={task}
-                  />
-               <button 
-                  className={ editMode ?  'btn btn-warning btn-block' : 'btn btn-dark btn-block' }
-                  type="submit"
-                >
-                  {editMode ? "Guardar" :"Agregar" }
-               </button>
-            </form>
-          </div>
+              </ul>
+            )
+          }
         </div>
+        <div className='col-4'>
+          <h4 className='text-center'
+          > {editMode ? "Modificar Tarea" : "Agregar Tarea"}
+          </h4>
+          <form onSubmit={editMode ? saveTask : addTask}>
+            {
+              error && <span className='text-danger'>{error}</span>
+            }
+            <input
+              className='form-control mb-2'
+              type="text"
+              placeholder='Ingrese la tarea'
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button
+              className={editMode ? 'btn btn-warning btn-block' : 'btn btn-dark btn-block'}
+              type="submit"
+            >
+              {editMode ? "Guardar" : "Agregar"}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
